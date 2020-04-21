67d196bc06b6a8c1800dd8d72ef8f8bef08e42f4
BENJAMIN NGARAMBE <kukibyanze@gmail.com>
2020-04-21 21:29:32 +0200

    almost clean random array generator

--- Program.cs
+++ Program.cs
@@ -8,76 +8,96 @@ namespace Project01
 {
     class Program
     {
-        public static int[] GenerateRandomArray(int size)
+        //Random array generator function
+        public static int [] GenerateRandomArray(int size)
         {
-            Random random = new Random();
+            //variable initialisation
             int Min = 0;
             int Max = 20;
-            int[] size = new int[1];
-            for (int i = 0; i < size.Length; i++)
-            {
-                size[i] = random.Next(Min, Max);
-            }
-        }
-        static bool SimpleLinearSearch(int[] array, int searchedValue)
-        {
-            int result = -1;
-            int a = array.Length;
-            int b = searchedValue;
-            for (int i = 0; i < a; i++)
-            {
-                if (array[i] = b)
-                {
-                    Console.WriteLine(i);
-                    return result;
-                }
-            }
-        }
-        static bool ImprovedLinearSearch(int[] array, int searchedValue)
-        {
+            int[] test2 = new int[size];
+           
+            //generating random numbers to use inside the array
+            Random randNum = new Random();
 
-            int a = array.Length;
-            int b = searchedValue;
-            for (int i = 0; i < a; i++)
+            //for loop to generate arrays with 
+            for (int i = 0; i < test2.Length; i++)
             {
-                if (array[i] == b)
-                {
-                    return i;
-                }
-                else
-                {
-                    return -1;
-                }
+                test2[i] = randNum.Next(Min, Max);
             }
+            for (int k = 0; k < test2.Length; k++)
+            { 
+                Console.WriteLine("\n" + test2[k] );
+            }
+            return test2;
         }
-        static bool ImprovedLinearSearchWithSentinel(int[] array, int searchedValue)
-        {
-            int a = array.Length;
-            int b = searchedValue;
-            int n = 1;
-            int last = array[n - 1];
-            array[n - 1] = b;
-            int i = 0;
-            while (array[i] != b)
-            {
-                i++;
-            }
-            array[n - 1] = last;
-            if (i < a || last = i)
-            {
-                return i;
-            }
-            else
-            {
-                return -1;
-            }
-        }
+        /* static bool SimpleLinearSearch(int[] array, int searchedValue)
+         {
+             int result = -1;
+             int a = array.Length;
+             int b = searchedValue;
+             for (int i = 0; i < a; i++)
+             {
+                 if (array[i] = b)
+                 {
+                     Console.WriteLine(i);
+                     return result;
+                 }
+             }
+         }
+         static bool ImprovedLinearSearch(int[] array, int searchedValue)
+         {
+
+             int a = array.Length;
+             int b = searchedValue;
+             for (int i = 0; i < a; i++)
+             {
+                 if (array[i] == b)
+                 {
+                     return i;
+                 }
+                 else
+                 {
+                     return -1;
+                 }
+             }
+         }
+         static bool ImprovedLinearSearchWithSentinel(int[] array, int searchedValue)
+         {
+             int a = array.Length;
+             int b = searchedValue;
+             int n = 1;
+             int last = array[n - 1];
+             array[n - 1] = b;
+             int i = 0;
+             while (array[i] != b)
+             {
+                 i++;
+             }
+             array[n - 1] = last;
+             if (i < a || last = i)
+             {
+                 return i;
+             }
+             else
+             {
+                 return -1;
+             }
+         }*/
         static void Main(string[] args)
         {
-            int[] arr = {1, 2, 3, 4, 5, 6, 7 };
-            int n = arr.Length;
-            int x = 5;
-            ImprovedLinearSearchWithSentinel(arr, x);
+            //prompting the user to write the number of elements of the array
+            Console.WriteLine("Enter the number of elements you want inside your array");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int loadingLength = 7;
+            Console.Write("Loading...");
+            for (int i = 0; i < loadingLength; i++) { 
+            Console.Write("");
+             }
+
+            //implementing the array to generate the random array
+            GenerateRandomArray(a);
+
+
         }
     }
 }
